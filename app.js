dc52162c92ca0824fedab02dac21fa667ae4b0d1
ferryamludn2 <ferryamludn@gmail.com>
2025-05-07 01:07:23 +0700

    chore: update fix Vercel routing configuration

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -12,10 +13,12 @@ const profileRoutes = require('./routes/profileRoutes');
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use('/api-docs', express.static(path.join(__dirname, 'node_modules/swagger-ui-dist')));
+
 app.use('/api', authRoutes);
 app.use('/api', profileRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
